f36fc2c3dea2011b5133ad5f304737ee59ed2306
Hristo Petrov <elemente365@gmail.com>
2019-02-21 14:32:13 +0000

    first commit

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "Uuhn7g-hbkY26Jjt";
+// MySQL Database Name
+$sql_db_name = "afktv_db1";
+
+// Site URL
+$site_url = "https://afktv.net"; // e.g (http://example.com)
+
+?>
---
+++ themes/default/info.php
@@ -0,0 +1,11 @@
+<?php
+$themeName = 'Default';
+
+$themeFolder = 'default';
+
+$themeAuthor = 'Hristo Petrov';
+
+$themeAuthorUrl = 'mailto:developers@afktv.net';
+
+$themeVirsion = '1.0';
+?>
